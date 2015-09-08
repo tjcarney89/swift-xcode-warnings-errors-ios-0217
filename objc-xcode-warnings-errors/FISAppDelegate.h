3398ba1f28061b69e6bffb6531bf2b0f9fc3c705
markedwardmurray <markedwardmurray@gmail.com>
2015-09-08 15:47:03 -0400

    Finish Setup

--- objc-xcode-warnings-errors/FISAppDelegate.h
+++ objc-xcode-warnings-errors/FISAppDelegate.h
@@ -1,10 +1,4 @@
-//
 //  FISAppDelegate.h
-//  objc-xcode-warnings-errors
-//
-//  Created by iOS Staff on 9/8/15
-//  Copyright (c) 2015 The Flatiron School. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
