a2642036b9d49e4b09eff396b6f1c3f4fc3150ea
markedwardmurray <markedwardmurray@gmail.com>
2015-09-08 15:32:36 -0400

    Initial Commit

---
+++ objc-xcode-warnings-errors/FISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FISAppDelegate.h
+//  objc-xcode-warnings-errors
+//
+//  Created by iOS Staff on 9/8/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ objc-xcode-warnings-errors/Resources/Other-Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc-xcode-warnings-errors
+//
+//  Created by iOS Staff on 9/8/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
