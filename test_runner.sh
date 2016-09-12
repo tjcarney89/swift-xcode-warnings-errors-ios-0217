a23029f88e959345f78793b0a11290265bedb35f
JimCampagno <jimcampagno@gmail.com>
2016-09-12 10:08:12 -0400

    Take out print in test runner

--- test_runner.sh
+++ test_runner.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-exec > /Users/JimCampagnoFIS/Desktop/my_log_file.txt 2>&1
 
 set -e
 SERVICE_URL='http://ironbroker.flatironschool.com'
 SERVICE_ENDPOINT='/e/flatiron_xcpretty'
 CURR_DIR="$2"
 NETRC=~/.netrc
-
-exec > /Users/JimCampagnoFIS/Desktop/my_log_file.txt 2>&1
-
 
 if [ -f ${NETRC} ]; then
   if grep -q flatiron-push ${NETRC}; then
@@ -30,9 +26,5 @@ FAILURE_COUNT=`tail -n10 unixfile.txt | grep -A1 xctest  | grep Executed | awk '
 PASSING_COUNT=`echo "${TOTAL_COUNT} - ${FAILURE_COUNT}" | bc`
 REPO_NAME=`echo ${CURR_DIR} | awk -F'/' '{print $NF}'`
 cd ${CURR_DIR}
-
-exec > /Users/JimCampagnoFIS/Desktop/my_log_file.txt 2>&1
-
-
 
 curl -s -H "Content-Type: application/json" -X POST --data "{ \"username\": \"${GITHUB_USERNAME}\", \"github_user_id\": \"${GITHUB_USER_ID}\", \"repo_name\": \"${REPO_NAME}\", \"build\": { \"test_suite\": [{\"framework\": \"xcpretty\", \"formatted_output\": [], \"duration\": 0.0, \"build_output\": []}]}, \"total_count\": ${TOTAL_COUNT}, \"passing_count\": ${PASSING_COUNT}, \"failure_count\": ${FAILURE_COUNT}}" "${SERVICE_URL}${SERVICE_ENDPOINT}"
